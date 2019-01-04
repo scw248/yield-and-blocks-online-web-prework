3a8b9ad9532ee5d2540d2373fd59f3b460a585b6
Stephen Wright <scw248@gmail.com>
2019-01-04 20:52:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,12 +8,12 @@ while i < array.length
 end
   array
 else
-  puts ""
+  puts "Hey! No block was given!"
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
